c1a26ed9d1503a8262280375fe10cafff7ec93a4
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-05-21 14:48:35 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+Route::get ('/', function()
+{
+    return redirect()->away('http://www.allowapp.com');
+});
 
 // Customer routes
 Route::group([
@@ -37,6 +30,29 @@ Route::group([
     Route::resource('acks', 'AcksController');
 });
 
+Route::group([
+    'domain' => '{location}.allowapp.test',
+    'middleware' => ['auth','location']], function ($location) {
+
+    Route::resource('/', 'HomeController');
+    Route::resource('users', 'UsersController');
+    Route::resource('profiles', 'ProfilesController');
+    Route::resource('groups', 'GroupsController');
+    Route::resource('persons', 'PersonsController');
+    Route::resource('locations', 'LocationsController');
+    Route::resource('rightholders', 'RightholdersController');
+    Route::get('groups/publicationsites/{id}', 'PublicationsitesController@index');
+    Route::resource('publicationsites', 'PublicationsitesController');
+    Route::resource('photos', 'PhotosController');
+    Route::get('photos/recognition/{id}',['uses' => 'PhotosController@recognition']);
+    Route::get('photos/run/{id}',['uses' => 'PhotosController@makeRecognition']);
+    Route::get('photos/contracts/{id}',['uses' => 'PhotosController@contracts']);
+    Route::get('addContract/{photo}/{person}',['uses' => 'PhotosController@addContract']);
+    Route::get('deleteContract/{contract}',['uses' => 'PhotosController@deleteContract']);
+
+    Route::resource('contracts', 'ContractsController');
+    Route::resource('acks', 'AcksController');
+});
 
 Route::get('/validatephoto/id={id}&ack={ack}&token={token}','ValidateController@accept');
 Route::get('/rejectphoto/id={id}&ack={ack}&token={token}','ValidateController@reject');
