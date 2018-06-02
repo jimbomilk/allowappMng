31c827cd2f3cd0703224599e988ebe9534b52004
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-06-02 23:40:12 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ if (env('APP_ENV') === 'production') {
     Route::get('/', function () {
         if (url()->current() == 'https://allowapp.com');
             return redirect()->away('https://www.allowapp.com/');
+        else
+            return redirect()->away('http://colegio1.allowapp.com/home');
     });
 }
 
