79d34188a95e315404c49f8b14eda0fb2b983967
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-06-04 16:59:29 +0200

    ok

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -1,4 +1,5 @@
 @include("common.controls.input_text",array('var'=>'name'))
 @include("common.controls.input_text",array('var'=>'email'))
+@include("common.controls.input_text",array('var'=>'phone'))
 @include("common.controls.input_password",array('var'=>'password'))
 @include("common.controls.input_password",array('var'=>'password2'))
