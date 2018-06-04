5c8531e5acdaab47ac88511824c18bb499a23fa5
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-06-04 16:56:08 +0200

    ok

--- app/User.php
+++ app/User.php
@@ -39,9 +39,7 @@ class User extends Authenticatable
 
     public function checkRole($reference)
     {
-        $type = Auth::user()->profile->type;
-
-        if ($this->roleVal($type) > $this->roleVal($reference)) {
+        if ($this->roleVal($this->profile->type) > $this->roleVal($reference)) {
             return true;
         }
         return false;
