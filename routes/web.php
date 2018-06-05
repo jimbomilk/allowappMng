866f101b9e0865f10e7f6b34892105e2b00543a2
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-06-05 18:22:21 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -9,30 +9,42 @@ Route::get('/', function(){
 // Customer routes
 
 Route::group([
-    'domain' => '{location}.allowapp.eu',
+    'domain' => '{location}.allowapp.test',
     'middleware' => ['auth','location']], function ($location) {
-        Route::resource('/', 'HomeController');
-        Route::resource('users', 'UsersController');
-        Route::resource('profiles', 'ProfilesController');
-        Route::resource('groups', 'GroupsController');
-        Route::resource('persons', 'PersonsController');
-        Route::resource('locations', 'LocationsController');
 
-        Route::resource('rightholders', 'RightholdersController');
-        Route::get('rightholders/consentimientos/all', ['uses'=>'RightholdersController@consentimientos']);
-        Route::get('rightholders/consentimientos/{id}', ['uses'=>'RightholdersController@consentimientos']);
-        Route::post('rightholders/emails',['uses' => 'RightholdersController@emails']);
+    Route::resource('/', 'HomeController');
+    Route::resource('users', 'UsersController');
+    Route::resource('profiles', 'ProfilesController');
+    Route::resource('groups', 'GroupsController');
+    Route::resource('persons', 'PersonsController');
+    Route::resource('locations', 'LocationsController');
+    Route::get('historic/persons', 'HistoricController@indexPersons');
+    Route::get('historic/persons/show/{id}', 'HistoricController@showPerson');
 
-        Route::get('groups/publicationsites/{id}', 'PublicationsitesController@index');
-        Route::resource('publicationsites', 'PublicationsitesController');
-        Route::resource('photos', 'PhotosController');
-        Route::get('photos/recognition/{id}',['uses' => 'PhotosController@recognition']);
-        Route::get('photos/run/{id}',['uses' => 'PhotosController@makeRecognition']);
-        Route::get('photos/send/{id}',['uses' => 'PhotosController@send']);
-        Route::post('photos/emails',['uses' => 'PhotosController@emails']);
-        Route::get('addContract/{photo}/{person}',['uses' => 'PhotosController@addContract']);
-        Route::get('deleteContract/{photo}/{person}',['uses' => 'PhotosController@deleteContract']);
-        Route::get('photos/share/{id}/{share}',['uses' => 'PhotosController@share']);
+    Route::get('historic/photos', 'HistoricController@indexPhotos');
+    Route::get('historic/photos/show/{id}', 'HistoricController@showPhoto');
+
+    Route::get('historic/rightholders', 'HistoricController@indexRightholders');
+    Route::get('historic/rightholders/show/{id}', 'HistoricController@showRightholder');
+
+
+
+    Route::resource('rightholders', 'RightholdersController');
+    Route::get('rightholders/consentimientos/all', ['uses'=>'RightholdersController@consentimientos']);
+    Route::get('rightholders/consentimientos/{id}', ['uses'=>'RightholdersController@consentimientos']);
+    Route::post('rightholders/emails',['uses' => 'RightholdersController@emails']);
+
+    Route::get('groups/publicationsites/{id}', 'PublicationsitesController@index');
+    Route::resource('publicationsites', 'PublicationsitesController');
+    Route::resource('photos', 'PhotosController');
+    Route::get('photos/recognition/{id}',['uses' => 'PhotosController@recognition']);
+    Route::get('photos/run/{id}',['uses' => 'PhotosController@makeRecognition']);
+    Route::get('photos/send/{id}',['uses' => 'PhotosController@send']);
+    Route::post('photos/emails',['uses' => 'PhotosController@emails']);
+    Route::get('addContract/{photo}/{person}',['uses' => 'PhotosController@addContract']);
+    Route::get('deleteContract/{photo}/{person}',['uses' => 'PhotosController@deleteContract']);
+    Route::get('photos/share/{id}/{share}',['uses' => 'PhotosController@share']);
+
 
 });
 
