bd1793d3eca0c6d584ba41016974680c90d46c4c
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-06-12 14:27:52 +0200

    ok

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -47,12 +47,15 @@ class LinkController extends Controller
             $photo = Photo::find($photoId);
             if (isset($photo)){
                 $sh= $photo->getData('sharing');
+
                 $sharing = [];
                 foreach ($sh as $share){
-                    $sharing[]=[$share->name=>(int)$req->get($share->name,'0')];
+                    $sharing[$share->name]=(int)$req->get($share->name,'0');
                 }
                 $photoData  = new PhotoData($photo->data);
+
                 if ($rh = $photoData->setRightholderSharing($dni,$rightholderId,$sharing)){
+
                     if ($photoData->allRightholdersProcessed())
                         $photoData->status = Status::STATUS_PROCESED;
                     $photo->data = json_encode($photoData);
@@ -105,27 +108,42 @@ class LinkController extends Controller
                         $pixelate[] = $face->Face->BoundingBox;
 
                     }else{
-                        $global_consents = false;
+                        $global_consents = true;
                         //3. Comprobamos los permisos globales del los rightholders para esa red
-                        foreach($person->rightholders as $rh){
-                            $rightholder = Rightholder::find($rh->id);
-
-                            if (isset($rightholder) && isset($rightholder->consent)){
-                                $consents = json_decode($rightholder->consent);
-
-                                $global_consents = $global_consents && $consents->$network;
-                            }
-                        }
-                        if (!$global_consents){
-                            //4. Comprobar los permisos de foto de los rightholders de la persona en esa red
-                            $local_consents = false;
-
-                            foreach($person->rightholders as $rh) {
-
-                                if (isset($rh->sharing)){
-                                    $local_consents = $local_consents && $rh->sharing->$network;
+                        if (isset($person->rightholders) && count($person->rightholders)>0) {
+                            foreach ($person->rightholders as $rh) {
+                                $rightholder = Rightholder::find($rh->id);
+                                if (isset($rightholder) && isset($rightholder->consent)) {
+                                    $consents = json_decode($rightholder->consent);
+                                    if (isset($consents->$network))
+                                        $global_consents = $global_consents && $consents->$network;
+                                    else
+                                        $global_consents = false;
+                                } else {
+                                    $global_consents = false;
                                 }
                             }
+                        }else{
+                            $global_consents = false;
+                        }
+                        if (!$global_consents){
+
+                            //4. Comprobar los permisos de foto de los rightholders de la persona en esa red
+                            $local_consents = true;
+                            if (isset($person->rightholders) && count($person->rightholders)>0) {
+                                foreach($person->rightholders as $rh) {
+
+                                    if (isset($rh->sharing) && isset($rh->sharing->$network)){
+                                        $local_consents = $local_consents && $rh->sharing->$network;
+                                    }else{
+                                        $local_consents = false;
+                                    }
+                                }
+
+                            }else{
+                                $local_consents = false;
+                            }
+
                             if (!$local_consents)
                                 $pixelate[] = $face->Face->BoundingBox;
                             // FIN
