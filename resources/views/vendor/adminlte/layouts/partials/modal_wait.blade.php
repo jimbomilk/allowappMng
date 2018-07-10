b7ee2b2a840e29c1a2b87438b59285ba6efb36dc
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-07-10 17:35:27 +0200

    ok

--- resources/views/vendor/adminlte/layouts/partials/modal_wait.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/modal_wait.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="modal">
-    <div class="modal-dialog">
+    <div class="modal-dialog text-left">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">
