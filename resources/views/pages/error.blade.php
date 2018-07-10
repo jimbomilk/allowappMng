af997c1e928f678ea8d6ccb310a299f80423a3ed
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-07-10 18:52:36 +0200

    ok

--- resources/views/pages/error.blade.php
+++ resources/views/pages/error.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <h1 style="text-align: center;color:white">
-    Se ha producido un error. El enlace es incorrecto.
+    Se ha producido un error. El enlace ya no existe o es incorrecto.
     </h1>
 
 @stop
