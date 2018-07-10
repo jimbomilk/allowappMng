60c8f267c5c2d20f66cd35f78a6dc087ad06feab
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-07-10 18:46:01 +0200

    ok

--- resources/views/pages/shared.blade.php
+++ resources/views/pages/shared.blade.php
@@ -9,10 +9,11 @@
     <meta property="og:description"        content="How much does culture influence creative thinking?" />
     <meta property="og:image"              content="{{$photo->url}}" />
 </head>
-<body>
+<body style="background-image: url('/images/background_allowapp.png')">
 <div style="width: 100%;text-align: center">
-<img style="text-align: center" class="img-responsive center" src="{{$photo->url}}">
-
+<img class="img-responsive center" src="{{$photo->url}}">
+<p ><small>Imagen compartida por <strong>{{$photo->photo->getData('accountable')}}</strong> como responsable del <strong>{{$photo->photo->location->description}}</strong>.<br> Si desea ejecer sus derechos de acceso, rectificación, cancelación u oposición debe
+enviar un correo electrónico a: <br> <strong> {{$photo->photo->getData('accountable_email')}}</strong>.<br> Indicando su nombre y dni, así como esta referencia a la imagen: <strong>{{$photo->photo->group->name}}#{{$photo->photo->getData('name')}}</strong></small></p>
 </div>
 </body>
 </html>
