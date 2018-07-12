b04baeedd752cdd2aa2c1531c28ebf37c408f9d9
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-07-12 06:49:48 +0200

    ok

--- resources/views/common/searcher.blade.php
+++ resources/views/common/searcher.blade.php
@@ -1,7 +1,7 @@
 
 <div style="float: right">
     {!! Form::model(Request::all(), array('url' => str_replace(".","/",$name), 'method' => 'GET', 'enctype' => 'multipart/form-data')) !!}
-    <div  class="col-md-8 col-xs-8 nopadding" style="margin:0">
+    <div  class="col-md-8 col-xs-7 nopadding" style="margin:0">
         {!! Form::text('search', null, ['class' => 'search-control', 'placeholder'=>trans("labels.search.$name")]) !!}
     </div>
     <div  class="col-md-1 col-xs-1 nopadding"  >
