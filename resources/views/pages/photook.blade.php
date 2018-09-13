d59192ba9f46fd1f56f8f3d0ace09934811b125a
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-09-13 11:19:48 +0200

    ok

--- resources/views/pages/photook.blade.php
+++ resources/views/pages/photook.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h2 style="text-align: center;color: white">
-                    Sus datos se han guardado correctamente , muchas gracias!
+                    Sus datos se han guardado correctamente, muchas gracias!
                 </h2>
             </div>
         </div>
