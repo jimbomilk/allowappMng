b81dc65c16a2c54d01574307c930125ce87e14c6
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-09-17 11:04:52 +0200

    ok

--- resources/views/pages/partials/basic.blade.php
+++ resources/views/pages/partials/basic.blade.php
@@ -6,11 +6,10 @@
         Ha recibido este enlaces como responsable de los derechos de imagen de <strong> {{$person_name}} </strong> <br>
 
         <div class="col-sm-12 informacion-basica" style="text-align: justify"><br>
-            <div><strong>{{trans('labels.responsable')}}: </strong>{{$photo->getData('accountable')}} con CIF {{$photo->getData('accountable_CIF')}}</div><br>
-            <div><strong>{{trans('labels.finalidad')}}: </strong>{{trans('label.finalidad_text')}} {{$photo->getSharingAsText()}}</div><br>
-            <strong>{{trans('labels.legitimacion')}}: </strong>{!!$photo->getData('consent_legitimacion')!!}
-            <strong>{{trans('labels.destinatario')}}: </strong>{!!$photo->getData('consent_destinatarios')!!}
-            <strong>{{trans('labels.derechos')}}: </strong>{!!$photo->getData('consent_derechos')!!}
+            <div><strong>{{trans('labels.responsable')}}: </strong>{{$photo->getData('accountable')}} con CIF {{$photo->getData('accountable_CIF')}}</div>
+            <div><strong>{{trans('labels.finalidad')}}: </strong>{{trans('label.finalidad_text')}} {{$photo->getSharingAsText()}}</div>
+            <div><strong>{{trans('labels.destinatario')}}: </strong>{!!$photo->getData('consent_destinatarios')!!}</div>
+            <div><strong>{{trans('labels.derechos')}}: </strong>{!!$photo->getData('consent_derechos')!!}</div>
         </div>
     </div>
 </div>
