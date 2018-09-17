82a3e602413e16abf8046b62df4d8a9a011601a0
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-09-17 14:48:18 +0200

    ok

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -55,7 +55,7 @@
                                     <a href="{{ url("tasks/$task->id/edit") }}">
                                         <!-- Task title and progress text -->
                                         <div class="col-sm-10 col-xs-10 " >
-                                            <h5 style="color: black">{{$task->description}}</h5>
+                                            <small style="color: black;line-height: 1.2;">{{$task->description}}</small>
                                         </div>
                                         <div class="col-sm-2 col-xs-2 nopadding " >
                                             @if($task->priority <=10)
