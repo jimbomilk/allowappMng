826bf583107c606d4956b22e8ca793e63eae6694
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2018-09-17 11:03:01 +0200

    ok

--- resources/views/pages/partials/basic.blade.php
+++ resources/views/pages/partials/basic.blade.php
@@ -5,7 +5,7 @@
     <div class="panel-body small" style="margin: 0px;height:50vh;overflow-y: scroll;overflow-x: hidden">
         Ha recibido este enlaces como responsable de los derechos de imagen de <strong> {{$person_name}} </strong> <br>
 
-        <div class="col-sm-12" style="text-align: justify"><br>
+        <div class="col-sm-12 informacion-basica" style="text-align: justify"><br>
             <div><strong>{{trans('labels.responsable')}}: </strong>{{$photo->getData('accountable')}} con CIF {{$photo->getData('accountable_CIF')}}</div><br>
             <div><strong>{{trans('labels.finalidad')}}: </strong>{{trans('label.finalidad_text')}} {{$photo->getSharingAsText()}}</div><br>
             <strong>{{trans('labels.legitimacion')}}: </strong>{!!$photo->getData('consent_legitimacion')!!}
